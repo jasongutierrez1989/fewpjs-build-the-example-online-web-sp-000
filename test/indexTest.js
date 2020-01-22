a6b30504f97a87f0d2ca61793961456b94c126e5
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-22 06:59:02 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,3 +4,10 @@ const chai = require( 'chai' );
 const spies = require( 'chai-spies' );
 
 chai.use( spies );
+
+describe( "main.js", () => {
+  it("contains a hidden modal", () => {
+    let modal = document.querySelector('.hidden')
+    expect(modal).not.to.equal(null)
+  } )
+} )
